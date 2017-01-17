6be102567a33a6fb9ea028bf9bf1dfbb2f9553fc
qianbw <biweiqian@webank.com>
2017-01-17 16:09:22 +0800

    Courier只写一个表中

--- src/test/java/com/example/CommonServiceTest.java
+++ src/test/java/com/example/CommonServiceTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import com.example.common.dto.CourierDTO;
+import com.example.service.CommonService;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
@@ -12,8 +16,19 @@ import org.springframework.test.context.junit4.SpringRunner;
         locations = {"classpath:application.properties"})
 public class CommonServiceTest extends AbstractJUnit4SpringContextTests {
 
+    @Autowired
+    @Qualifier("com.example.service.CommonService")
+    private CommonService commonService;
+
     @org.junit.Test
     public void tests1() {
-        System.out.println("1111111");
+        CourierDTO courierDTO = new CourierDTO();
+
+        for (int loopIdx = 1; loopIdx < 10; loopIdx++) {
+            courierDTO.setName(String.valueOf(loopIdx));
+            courierDTO.setId(String.valueOf(loopIdx));
+            int result = commonService.addCourier(courierDTO);
+            System.out.println(String.valueOf(result));
+        }
     }
 }
